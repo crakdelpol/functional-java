effbe4cb697310f27980a45fe336d4979ae1ed65
Matteo <matteo.pipitone93@gmail.com>
2020-03-11 07:58:07 +0100

    Learn Supplier

--- src/_Supplier.java
+++ src/_Supplier.java
@@ -1,6 +1,20 @@
+import java.util.List;
+import java.util.function.Supplier;
+
 public class _Supplier {
 
     public static void main(String[] args) {
         System.out.println("let's start with SUPPLIER");
+
+        System.out.println(getConfiguration());
+        System.out.println(getConfigurationSupplier.get());
+        System.out.println(getConfigurationsSupplier.get());
     }
+
+    static String getConfiguration(){
+        return "configuration";
+    }
+
+    static Supplier<String> getConfigurationSupplier = () -> "configuration";
+    static Supplier<List<String>> getConfigurationsSupplier = () -> List.of("configuration","configuration2");
 }
